00a2402e237f63c02ab340ceae35e2e69f88824a
yaches <@>
2017-05-12 15:11:13 +0300

    fix

--- exploders/src/SchedulerService.java
+++ exploders/src/SchedulerService.java
@@ -10,23 +10,21 @@ public class SchedulerService {
 
     private AtomicLong serverTime = new AtomicLong(0);
 
-    private TreeMap<Long, Job> awaitingJobs = new TreeMap<>();
+    private SortedMap<Long, Job> awaitingJobs = new TreeMap<>();
 
     public void gmTick(long frameMillis) {
         long currentTime = serverTime.addAndGet(frameMillis);
-//        SortedMap<Long, Job> jobsToExecute = awaitingJobs.tailMap(currentTime);
-        SortedMap<Long, Job> jobsToExecute = awaitingJobs.subMap((long) 0, currentTime);
+        SortedMap<Long, Job> jobsToExecute = awaitingJobs.headMap(currentTime);
         for (Map.Entry<Long, Job> longJobEntry : jobsToExecute.entrySet()) {
             //some code here
-            long time = longJobEntry.getKey();
             Job job = longJobEntry.getValue();
             try {
                 job.execute();
             } catch (Exception e) {
                 e.printStackTrace();
             }
-            awaitingJobs.remove(time);
         }
+        awaitingJobs = awaitingJobs.tailMap(currentTime);
         System.out.println("tick-tock. Time is " + serverTime);
     }
 
